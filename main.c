ff6a15dee0561c08dc2008d0240b39d1489753d9
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-08-31 17:30:09 +0800

    第三次修改main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include"stdio.h"
 #include"stdlib.h"
 #include"math.h"
+#include"time.h"
 int main(void)
 {
-    double n;
+    int n;
     n=abs(100*sin(rand()));
     if(n<50)
-        printf("bigger than 50,n=%d\n",&n);
+        printf("bigger than 50,n=%d\n",n);
     else
-        printf("smaller than 50,n=%d\n");
+        printf("smaller than 50,n=%d\n",n);
     system("pause");
     return 0;
 }
