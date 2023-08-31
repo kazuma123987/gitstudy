161ac6388d771a3706c3d3ee2fead4d1dc5651a7
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-08-31 20:50:39 +0800

    none

---
+++ main.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"math.h"
+#include"time.h"
+int main(void)
+{
+    int n;
+    srand(time(NULL));
+    n=abs(100*sin(rand()));
+    if(n<50)
+        printf("bigger than 50,n=%d\n",n);
+    else
+        printf("smaller than 50,n=%d\n",n);
+    system("pause");
+    return 0;
+}
