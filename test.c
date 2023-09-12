f724850018ff9ceafd40126a26285f48a3abb126
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-12 19:01:19 +0800

    数字排列

--- test.c
+++ test.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-void printGCD()
+void printnum()
 {
-    int a, b, t;
-    printf("请输入a和b的值：\n");
-    scanf("%d %d", &a, &b);
-    while (b != 0)
+    int a,i,j,k;
+    printf("请输入a(a为不大于6的正整数)的值：\n");
+    scanf("%d", &a);
+    for(i=a;i<a+4;i++)
     {
-        t = a % b;
-        a = b;
-        b = t;
+    printf("\n");
+    for(j=a;j<a+4;j++)
+    for(k=a;k<a+4;k++)
+    if(i!=j&&j!=k&&k!=i)printf("%d,",i*100+j*10+k);
     }
-    printf("a和b的最大公约数是:%d\n", a);
 }
 int main()
 {
-    printGCD();
+    printnum();
     return 0;
 }
