e7cec154765002698ee931c45c7d1c7ef6ab6727
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-13 21:06:04 +0800

    素数之和

--- test.c
+++ test.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-void printform()
+void pn_func()
 {
-    for(int i=1;i<=9;i++)
+    int m,n,sum=0,count=0;
+    printf("输入m与n的值：");
+    scanf("%d %d",&m,&n);
+    for(int i=m;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)printf("%d*%d=%-4d",j,i,i*j);//%-4d是左对齐，%4d是右对齐，一共4个空位
-        printf("\n");
+        int num=0;
+        for(int j=2;j<=sqrt(i);j++)//平方根优化循环次数
+        {
+            if(i%j==0)num++;
+        }
+        if(num==0)
+        {
+            sum+=i;
+            count++;
+        }
     }
+    printf("%d到%d的素数个数为：%d,其和为：%d",m,n,count,sum);
 }
 
 int main()
 {
-    printform();
+    pn_func();
     return 0;
 }
