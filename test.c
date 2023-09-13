51309c68ff2bc2d96e6376d510b11abe76f9f3d7
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-13 20:30:25 +0800

    九九乘法表（左对齐%-4d）

--- test.c
+++ test.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-void printnum()
+void printform()
 {
-    int N, i, j, t, num, num1, sum;
-    printf("请输入水仙花数的位数(3到7之间)：\n");
-    scanf("%d", &N);
-    num = pow(10, N - 1);
-    //用数组简化n次幂运算步骤
-    int power_of_digits[10];
-    for (i = 0; i < 10; i++)
+    for(int i=1;i<=9;i++)
     {
-        power_of_digits[i] = pow(i, N);
-    }
-
-    for (i = num; i < num * 10; i++)
-    {
-        sum = 0;
-        t = i;
-        num1 = num;
-        for (j = 1; j <= N; j++)
-        {
-            sum += power_of_digits[t / num1];
-            t %= num1;
-            num1 /= 10;
-        }
-        if (sum == i)
-            printf("%d\n", sum);
+        for(int j=1;j<=i;j++)printf("%d*%d=%-4d",j,i,i*j);//%-4d是左对齐，%4d是右对齐，一共4个空位
+        printf("\n");
     }
 }
 
 int main()
 {
-    printnum();
+    printform();
     return 0;
 }
