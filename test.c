679722d94067be054d0eb630664563d42edfeade
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-15 20:15:55 +0800

    分数前N项求和

--- test.c
+++ test.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
-void pn_func()
+void frac_sum()
 {
-    int m,n,sum=0,count=0;
-    printf("输入m与n的值：");
-    scanf("%d %d",&m,&n);
-    for(int i=m;i<=n;i++)
+    int N,t=0,num1=2,num2=1;
+    double sum=0;
+    printf("输入N的值：");
+    scanf("%d",&N);
+    for(int i=1;i<=N;i++)
     {
-        int num=0;
-        for(int j=2;j<=sqrt(i);j++)//平方根优化循环次数
-        {
-            if(i%j==0)num++;
-        }
-        if(num==0)
-        {
-            sum+=i;
-            count++;
-        }
+        sum+=num1*1.0/num2;
+        t=num2;
+        num2=num1;
+        num1+=t;
     }
-    printf("%d到%d的素数个数为：%d,其和为：%d",m,n,count,sum);
+    printf("2/1+3/2+5/3+8/5...的前%d项和为：%.2lf\n",N,sum);
 }
 
 int main()
 {
-    pn_func();
+    frac_sum();
     return 0;
 }
