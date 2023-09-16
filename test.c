85583192d941bf46ad6117b2f3d0373ac4b465a6
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 16:57:26 +0800

    逻辑运算符的短路

--- test.c
+++ test.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 void fun()
 {
-    //光标回退\b
-    printf("光标回退:\n");
-    printf("123\b\n");
-    printf("123\b456\n");
-    printf("%d",sizeof("123\b456"));//注意在sizeof里面一切运算无效，包括a++，\n换行等
-    printf("\n");
-    //光标到下一个表格位\t
-    printf("光标到下一个表格位:\n");
-    printf("1234\t56\n");
-    printf("123\t45\n");
-    //光标到当前行最开始处\r
-    printf("光标到当前行最开始处:\n");
-    printf("1234\r56\n");
-    printf("1234\r\n");
+    int a=0,b=2,c=2;
+    if((a>0) || (b+=1))a++;
+    printf("a=%d,b=%d\n",a,b);
+    if((a==0) || (b+=1))a++;
+    printf("a=%d,c=%d\n",a,c);    
 }
 int main()
 {
