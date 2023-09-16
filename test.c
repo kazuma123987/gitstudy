b483786438f1001a36d4aa3fa1f11df9b46c00f5
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 20:42:39 +0800

    sizeof(数组)=sizeof(数组类型)*数组大小

--- test.c
+++ test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 void fun()
 {
-    int a=0,b=2,c=2;
-    if((a>0) || (b+=1))a++;
-    printf("a=%d,b=%d\n",a,b);
-    if((a==0) || (b+=1))a++;
-    printf("a=%d,c=%d\n",a,c); 
-    //说明在写代码时避免把复合赋值写在逻辑运算中   
+    int a[100];
+    char str[100];
+    printf("sizeof(a)=%d,sizeof(str)=%d",sizeof(a),sizeof(str));   
 }
 int main()
 {
