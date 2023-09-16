533573bb53904bce9abefbf0580b17023f41bbb4
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 13:28:36 +0800

    %u与%lu的用法及其特性

--- test.c
+++ test.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void num()
 {
-    char a=127;//char可表示整数，其范围为-128~127
-    unsigned char b=127;//unsigned char范围为0~255
-    a++;
-    b++;
-    printf("a=%d,b=%d\n", a,b);//对于char类型，127+1=-128（越界），-128-1=127
+    char a=-1;
+    int b=-1;
+    //编译器是32位所以长度均为4个字节
+    printf("sizeof(unsigned int)=%d,sizeof(unsigned long)=%d\n",sizeof(unsigned int),sizeof(unsigned long));
+    //%u和%lu分别输出（unsigned int）和（unsigned long）类型，在输出前会将输出前的值强行更换为unsigned长度的数字类型
+    printf("alu=%u,a1u=%lu,bu=%u,blu=%lu\n", a,a,b,b);
 }
-
 int main()
 {
     num();
