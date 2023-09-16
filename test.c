8f4574c1800f72fb928de9925a9b99221f5ed364
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 16:58:59 +0800

    逻辑运算符的短路

--- test.c
+++ test.c
@@ -5,7 +5,8 @@ void fun()
     if((a>0) || (b+=1))a++;
     printf("a=%d,b=%d\n",a,b);
     if((a==0) || (b+=1))a++;
-    printf("a=%d,c=%d\n",a,c);    
+    printf("a=%d,c=%d\n",a,c); 
+    //说明在写代码时避免把复合赋值写在逻辑运算中   
 }
 int main()
 {
