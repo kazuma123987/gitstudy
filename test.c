ed6686b5f904eaea5b1ab44830d385b4f4b91197
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 16:34:57 +0800

    转义字符

--- test.c
+++ test.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-void num()
+void fun()
 {
-    float a, b, c;
-    a = 0.112;
-    b = 0.231;
-    c = a + b;
-    if (c == 0.343) // float c精度为小数点后7位，与精度为小数点后15位的(double)（0.343）相比，很大概率不相等
-        printf("相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
-    else
-        printf("不相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
-    if (c == (float)0.343) // 将0.343强制转化为float类型两边则相等
-        printf("相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
-    else
-        printf("不相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
-    double x, y, z;
-    x = 0.112;
-    y = 0.231;
-    z = x + y;
-    if (z == 0.343) // 等式两边都是double类型(精度为小数点后15位)，其值相等
-        printf("相等,x=%.20f,y=%.20f,z=%.20f,0.343=%.20f\n", x, y, z, 0.343);
-    else
-        printf("不相等,x=%.20f,y=%.20f,z=%.20f,0.343=%.16f\n", x, y, z, 0.343);
+    //光标回退\b
+    printf("光标回退:\n");
+    printf("123\b\n");
+    printf("123\b456\n");
+    printf("%d",sizeof("123\b456"));//注意在sizeof里面一切运算无效，包括a++，\n换行等
+    printf("\n");
+    //光标到下一个表格位\t
+    printf("光标到下一个表格位:\n");
+    printf("1234\t56\n");
+    printf("123\t45\n");
+    //光标到当前行最开始处\r
+    printf("光标到当前行最开始处:\n");
+    printf("1234\r56\n");
+    printf("1234\r\n");
 }
 int main()
 {
-    num();
+    fun();
     return 0;
 }
