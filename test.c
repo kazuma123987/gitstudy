9aaa02214a43f2190e9f8486a363cf61903e86cd
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 21:16:21 +0800

    统计数字输入次数

--- test.c
+++ test.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 void fun()
 {
-    int a[100];
-    char str[100];
-    printf("sizeof(a)=%d,sizeof(str)=%d",sizeof(a),sizeof(str));   
+    int n = 0;
+    int count[10];
+    for(int i=0;i<10;i++)count[i]=0;
+    printf("input a number(0-9),the program will stop when you input '-1'\n");
+    scanf("%d", &n);
+    while (n != -1)
+    {
+        if (n > 9 || n < 0)
+        {
+            printf("input 0-9 or -1!!!\n");
+        }
+        else count[n]++;
+        scanf("%d", &n);
+    }
+    for(int i=0;i<10;i++)
+    {
+        printf("输入%d的个数:\t%d\n",i,count[i]);
+    }
 }
 int main()
 {
