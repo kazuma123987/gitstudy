6122ea0be2fb5bed2967fd92f3e9639a313c02d5
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-16 15:04:09 +0800

    float与double精度问题与解决方法

--- test.c
+++ test.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 void num()
 {
-    char a=-1;
-    int b=-1;
-    //编译器是32位所以长度均为4个字节
-    printf("sizeof(unsigned int)=%d,sizeof(unsigned long)=%d\n",sizeof(unsigned int),sizeof(unsigned long));
-    //%u和%lu分别输出（unsigned int）和（unsigned long）类型，在输出前会将输出前的值强行更换为unsigned长度的数字类型
-    printf("alu=%u,a1u=%lu,bu=%u,blu=%lu\n", a,a,b,b);
+    float a, b, c;
+    a = 0.112;
+    b = 0.231;
+    c = a + b;
+    if (c == 0.343) // float c精度为小数点后7位，与精度为小数点后15位的(double)（0.343）相比，很大概率不相等
+        printf("相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
+    else
+        printf("不相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
+    if (c == (float)0.343) // 将0.343强制转化为float类型两边则相等
+        printf("相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
+    else
+        printf("不相等,a=%.8f,b=%.8f,c=%.8f\n", a, b, c);
+    double x, y, z;
+    x = 0.112;
+    y = 0.231;
+    z = x + y;
+    if (z == 0.343) // 等式两边都是double类型(精度为小数点后15位)，其值相等
+        printf("相等,x=%.20f,y=%.20f,z=%.20f,0.343=%.20f\n", x, y, z, 0.343);
+    else
+        printf("不相等,x=%.20f,y=%.20f,z=%.20f,0.343=%.16f\n", x, y, z, 0.343);
 }
 int main()
 {
