5825da37fd8e963c11ada974db5f2127103ffd3d
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-17 18:01:49 +0800

    棋盘连线游戏

--- test.c
+++ test.c
@@ -1,38 +1,114 @@
-#include <stdio.h>
-
-int isPrime(int number, int prime[], int count)
+#include "stdio.h"
+#include "windows.h"
+#include "stdbool.h"
+#include "string.h"
+void find_line(int arrey_len,int arrey[arrey_len][arrey_len],bool x)
 {
-    int ret = 1;
-    for (int i = 0; i < count; i++)
+    char str[10];
+    if(x)strcpy(str,"O");
+    else strcpy(str,"X");
+  // ĞĞ±éÀú
+    for (int i = 0; i < arrey_len; i++)
     {
-        if (number % prime[i] == 0)
+        int t = x;
+        for (int j = 0; j < arrey_len; j++)
         {
-            ret = 0;
-            break;
+            if(x)t = (t && arrey[i][j]);
+            else t = (t || arrey[i][j]);
+            if (t == !x)
+                break;
         }
+        if (t == x)
+            printf("µÚ%dĞĞµÄ%sÁ¬³ÉÒ»Ïß\n",i+1,str);
     }
-    return ret;
+    // ÁĞ±éÀú
+    for (int i = 0; i < arrey_len; i++)
+    {
+        int t = x;
+        for (int j = 0; j < arrey_len; j++)
+        {
+            if(x)t = (t && arrey[j][i]);
+            else t = (t || arrey[j][i]);
+            if (t == !x)
+                break;
+        }
+        if (t == x)
+            printf("µÚ%dÁĞµÄ%sÁ¬³ÉÒ»Ïß\n",i+1,str);
+    }
+    //×ó¶Ô½ÇÏß±éÀú
+    for(int i=0,t=x;i<arrey_len;i++)
+    {
+        if(x)t=(t&&arrey[i][arrey_len-1-i]);
+        else t=(t||arrey[i][arrey_len-1-i]);
+        if(t==!x)break;
+        if((i==arrey_len-1)&&t==x)printf("×ó¶Ô½ÇÏßµÄ%sÁ¬³ÉÒ»Ïß\n",str);
+    }
+    //ÓÒ¶Ô½ÇÏß±éÀú
+    for(int i=0,t=x;i<arrey_len;i++)
+    {
+        if(x)t=(t&&arrey[i][i]);
+        else t=(t||arrey[i][i]);
+        if(t==!x)break;
+        if(i==arrey_len-1&&t==x)printf("ÓÒ¶Ô½ÇÏßµÄ%sÁ¬³ÉÒ»Ïß\n",str);
+    }   
+}
+void game()
+{
+    int N = 1;
+    printf("ÇëÊäÈëÆåÅÌµÄĞĞÊı£¨»òÁĞÊı£©N:(1<N<15)\n");
+    while (N <= 1 || N >= 15)
+    {
+        scanf("%d", &N);
+        if (N <= 1 || N >= 15)
+            printf("invalid number.\n");
+    }
+    int arrey[N][N];
+    printf("ÊäÈë%d¸öÊı×Ö(1»ò0)ÎªÊı×é¸³Öµ(ÊäÈë-1ÍË³öº¯Êı)£º\nÊäÈë1±íÊ¾O,ÊäÈë0±íÊ¾X\n", N * N);
+    int i = 0;
+    while (i != N)
+    {
+        int j = 0;
+        while (j != N)
+        {
+            scanf("%d", &arrey[i][j]);
+            if (arrey[i][j] == 1 || arrey[i][j] == 0)
+            {
+                j++;
+            }
+            else if(arrey[i][j]==-1)
+            {
+                printf("Exit successfully!\n");
+                return;
+            }
+            else
+                printf("input 1 or 0 !\n");
+        }
+        i++;
+    }
+    {
+        printf("ÆåÅÌÈçÏÂÍ¼ËùÊ¾£º\n");
+        printf("--------------------------begin--------------------------\n");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (arrey[i][j] == 1)
+                    printf("O ");
+                else
+                    printf("X ");
+            }
+            printf("\n");
+        }
+        printf("---------------------------end---------------------------\n");
+    }
+    find_line(N,arrey,true);
+    printf("---------------------------·Ö¸îÏß---------------------------\n");
+    find_line(N,arrey,false);
 }
 
 int main()
 {
-    const int maxsize = 100;
-    int prime[maxsize];
-    prime[0]=2;
-    int count = 1, i = 3;
-    while (count != maxsize)
-    {
-        if (isPrime(i, prime, count))
-        {
-            prime[count++] = i;
-        }
-        i++;
-    }
-    printf("è¿ç»­çš„%dä¸ªç´ æ•°:\n",maxsize);
-    for(int i=0;i<maxsize;i++)
-    {
-    printf("%d",prime[i]);
-    if(i<maxsize-1)printf(", ");
-    }
+    game();
+    system("pause");
     return 0;
 }
