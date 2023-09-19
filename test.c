5db1c0ec29ac7f5969c8c81a67e39d8a35d38918
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-19 15:42:43 +0800

    指针的强制类型转换

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 int main(void)
 {
-    /*补充说明tips:
-    1.a=*p++执行步骤:a=*p;p++
-    2.*p++的优点：对应于一条汇编语句，提高程序速度
-    */
     int a[2]={1,2};
     int *p=&a[0];
-    printf("%d\n",&a[1]-&a[0]);//地址相减后不会直接输出，而是相减后除以sizeof(地址对应的数据类型)
-    printf("%d\n",(p+1)-p);
+    char *q=(char *)p;
+    printf("%d\n",*q);
+    printf("%d\n",*(q+1));
+    printf("%d\n",*(q+2));
+    printf("%d\n",*(q+3));
+    printf("%d\n",*(q+4));
 }
