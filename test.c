49e8ab791cdfd65ce4f9a7066618b5e71ad18453
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-19 14:15:39 +0800

    const与*补充

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 int main(void)
 {
+    //const和*同时出现时，如果是const *p,则const后面的*p不能被修改；如果是*const q,则const后面的q不能被修改
     int a=26;
     int const *p=&a;    //int const *q定义后的指针所指地址存放的值不能改变
     p++;                //指针所在地址可以变，但不能写做*p=2这样给所指的值赋值
