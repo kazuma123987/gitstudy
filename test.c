c6545a8bf7ceb19a4afe452f6a1f94fb37266099
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-19 14:51:23 +0800

    函数改变指针的地址与int const在数组传递的使用

--- test.c
+++ test.c
@@ -1,13 +1,17 @@
 #include "stdio.h"
+void fun(int* (*q))//对传递的指针地址进行改变的方式
+{
+    (*q)++;
+}
+void f(int const a[])//使用int const可以保证数组里面的元素不被改变，因为数组也是一种指针
+{
+    a++;
+}
 int main(void)
 {
-    //const和*同时出现时，如果是const *p,则const后面的*p不能被修改；如果是*const q,则const后面的q不能被修改
-    int a=26;
-    int const *p=&a;    //int const *q定义后的指针所指地址存放的值不能改变
-    p++;                //指针所在地址可以变，但不能写做*p=2这样给所指的值赋值
-    printf("%p\n",&a);
-    printf("%p\n",p);
-    int* const q=&a;    //int* const q定义的指针的地址不能改变
-    *q=30;              //指针所值地址的值可以变，但不能写作q++给指针的地址赋值
-    printf("%d",a);
+    int a[2]={1,2};
+    f(a);
+    int *p=&a[0];
+    fun(&p);
+    printf("%d\n",*p);
 }
