74f58ca56459aefe6f7d6d0cd352c7ef81d86e90
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-19 15:25:31 +0800

    *p++的优点与地址的减法

--- test.c
+++ test.c
@@ -1,17 +1,12 @@
 #include "stdio.h"
-void fun(int* (*q))//对传递的指针地址进行改变的方式
-{
-    (*q)++;
-}
-void f(int const a[])//使用int const可以保证数组里面的元素不被改变，因为数组也是一种指针
-{
-    a++;
-}
 int main(void)
 {
+    /*补充说明tips:
+    1.a=*p++执行步骤:a=*p;p++
+    2.*p++的优点：对应于一条汇编语句，提高程序速度
+    */
     int a[2]={1,2};
-    f(a);
     int *p=&a[0];
-    fun(&p);
-    printf("%d\n",*p);
+    printf("%d\n",&a[1]-&a[0]);//地址相减后不会直接输出，而是相减后除以sizeof(地址对应的数据类型)
+    printf("%d\n",(p+1)-p);
 }
