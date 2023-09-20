f105a7727dabf1739cef1235326d0e0bdc792ade
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-20 18:50:11 +0800

    结构体补充

--- test.c
+++ test.c
@@ -10,14 +10,18 @@ typedef struct
 
 typedef struct
 {
-    char day;//偏移量0
-    int month;//4
-    double year;//8
-    time p;//16
-    float i;//104
-    char str[16];//108
+    char day;//偏移量0,占用字节1
+    int month;//4，占用字节4
+    double year;//8，占用字节8
+    time p;//16，占用字节88
+    float i;//104，占用字节4
+    char str[16];//108，占用字节16+4，4为补充的字节
 }date;//108+16=124,结构体最大字节sizeof(double)=8,8的倍数最接近124的为128
 int main()
 {
+    date d;
+    time t;
+    printf("%p\n",d);//首地址为最大类型的倍数
+    printf("%p\n",t);//首地址为最大类型的倍数,发现t和d首地址相同
     printf("sizeof(time)=%d,sizeof(date)=%d\n",sizeof(time),sizeof(date));
 }
