7ffabbe772537858752c105096f5655c885ab980
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-20 21:57:31 +0800

    子函数使用malloc的注意事项

--- test.c
+++ test.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#define f(a,b) a+b
+#include "stdlib.h"
+#include "string.h"
+void fun1(char **p)//注意用子函数为主函数分配空间时要用双重指针
+{
+   *p=(char *)malloc(100);
+}
+char *fun2()
+{
+   char *p=NULL;
+   p=(char*)malloc(100);
+   strcpy(p,"hello world 2");
+   return p;
+}
 int main()
 {
-   int a=4,b=6,c=4;
-   c=f(a,b)/f(a*b,b);//注意展开后为a+b/a*b+b=4+6/4*6+6=16
-   printf("%d",c);
+   char *str1=NULL,*str2=NULL;
+   fun1(&str1);
+   str2=fun2();//改变了指针的地址，指向了一个有足够空间的地址
+   strcpy(str1,"hello world 1");
+   printf("str1为:%s\nstr2为:%s\n",str1,str2);
+   free(str1);
+   free(str2);
 }
