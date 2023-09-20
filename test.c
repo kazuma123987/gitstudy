57e4a0dc2a2dfe595186513528bd462a624fbe73
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-20 16:14:44 +0800

    显示第二天日期（结构体）

--- test.c
+++ test.c
@@ -1,25 +1,70 @@
 #include <stdio.h>
 #include <string.h>
-
-void fgets_check(char *str, size_t size)
+struct date
 {
-    // 检查是否输入的字符串超过了最大长度
-    if (strlen(str) == size - 1 && str[size - 2] != '\n')
+    int day;
+    int month;
+    int year;
+};
+int is_leap_year(struct date d)
+{
+    int ret = 0;
+    if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
+        ret = 1;
+    return ret;
+}
+int Month_days(struct date d)
+{
+    int month_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (is_leap_year(d))
+        month_days[1] = 29;
+    return month_days[d.month - 1];
+}
+void date_input(struct date *d)
+{
+    printf("请输入今天的日期（月/日/年）：\n");
+    while (1)
     {
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF)
+        scanf("%d%d%d", &d->month, &d->day, &d->year);
+        if (d->day > Month_days(*d) || d->day <= 0)
         {
-            // 丢弃剩余的字符
+            printf("invalid day!\n");
+            continue;
         }
+        else if (d->month > 12 || d->month <= 0)
+        {
+            printf("invalid month!\n");
+            continue;
+        }
+        else if (d->year < 0)
+        {
+            printf("invalid year!\n");
+            continue;
+        }
+        break;
     }
 }
 int main()
 {
-    char str2[]="Abcd";//9-13
-    char str1[2];//11-12
-    printf("%p\n",str1);
-    printf("%p\n",str2);
-    printf("%s\n",strcpy(str1,str2));//注意定义时先定义str1,或者str1长度大于str2,否则会出现重合
-    printf("%s\n",str2);//重合后影响str2的值
-    return 0;
+    struct date today, tomorrow;
+    date_input(&today);
+    if (today.day < Month_days(today))
+    {
+        tomorrow.day = today.day + 1;
+        tomorrow.month = today.month;
+        tomorrow.year = today.year;
+    }
+    else if (today.month == 12)
+    {
+        tomorrow.day = 1;
+        tomorrow.month = 1;
+        tomorrow.year = today.year + 1;
+    }
+    else
+    {
+        tomorrow.day = 1;
+        tomorrow.month = today.month + 1;
+        tomorrow.year = today.year;
+    }
+    printf("明天的日期是:%d\\%d\\%d\n", tomorrow.month, tomorrow.day, tomorrow.year);
 }
