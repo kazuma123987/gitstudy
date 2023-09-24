c999b3173e346c0acc5bffd1604bb4baa5222c5c
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-24 15:24:50 +0800

    链表新增函数与clock计时函数

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "string.h"
 #include "stdint.h"
+#include "time.h"
 typedef struct _node
 {
    int data;
@@ -13,22 +14,30 @@ typedef struct _list
    Node *tail;
 } List;
 void add_Node(List *list, int number);
-void list_print(Node *p);
-int myscanf_int(int *n, int size);
+void list_delete(List *list,int delete_num);
+void list_print(List *list);//直接传结构体其占用空间过大，传递指针只占用4-8个字节
+void myscanf_int(int *n, int size);
+void list_free(List *list);//直接传结构体其占用空间过大，传递指针只占用4-8个字节
 int main()
 {
+   clock_t start,stop;
+   double duration;
+   start=clock();
    int number = 0;
-   List list1;
-   list1.head = list1.tail = NULL;
-   while (1)
-   {
+   List list[2];
+   list[0].head = list[0].tail = NULL;
       printf("输入数据为链表赋值，-1表示停止赋值\n");
-      myscanf_int(&number, 9);
-      if (number == -1)
-         break;
-      add_Node(&list1, number);
-   }
-   list_print(list1.head);
+      add_Node(&list[0], 1);
+      add_Node(&list[0], 2);
+      add_Node(&list[0], 3);
+      add_Node(&list[0], 4);
+      add_Node(&list[0], 5);
+   list_delete(&list[0],3);
+   list_print(&list[0]);//直接传结构体其占用空间过大，传递指针只占用4-8个字节
+   list_free(&list[0]);//直接传结构体其占用空间过大，传递指针只占用4-8个字节 */
+   stop=clock();
+   duration=((double)(stop-start))/CLK_TCK;
+   printf("程序花费%llf秒\n",duration);
 }
 void add_Node(List *list, int number)
 {
@@ -45,15 +54,48 @@ void add_Node(List *list, int number)
    else //如果链表的尾部指向的是NULL，则将头部和尾部都连接到新节点
       list->tail = list->head = newNode;
 }
-void list_print(Node *p)
+void list_delete(List *list,int delete_num)
 {
-   while (p != NULL)
+   Node *current,*pre;//pre指向current之前的结构体
+   for(current=list->head,pre=NULL;current;pre=current,current=current->next)/*出循环的条件是current指向NULL，
+   每次出循环后pre指向current的结构体,current指向下一个结构体*/
+   {
+      if(current->data==delete_num)//找到了要删除的节点
+      {
+         if(pre==NULL)//如果要删除头节点
+         {
+            list->head=list->head->next;
+            free(current);
+         }
+         else
+         {
+            pre->next=current->next;
+            free(current);
+         }
+         break;
+      }
+   }
+}
+void list_print(List *list)
+{
+   Node *p=list->head;
+   while (p!= NULL)
    {
       printf("%d ", p->data);
       p = p->next;
    }
 }
-int myscanf_int(int *n, int size)
+void list_free(List *list)
+{
+   Node *p=list->head;
+   while(p!=NULL)
+   {
+   Node *current=p;
+   p=p->next;
+   free(current);
+   }  
+}
+void myscanf_int(int *n, int size)
 {
    if (size > 0)
       size++;
