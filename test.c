513f6d8ca6150184136c64ebba84769150432220
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-24 12:01:16 +0800

    链表数组的创建与使用

--- test.c
+++ test.c
@@ -1,32 +1,70 @@
 #include <stdio.h>
+#include "stdlib.h"
 #include "string.h"
 #include "stdint.h"
-/*-----------------------------------notice-----------------------------------
-
-所有类型左移都是空位(低位)补0，
-unsigned类型右移高位补0，而非unsigned类型(int ,char ,long等)右移高位保持不变
-
--------------------------------------notice---------------------------------*/
-typedef struct
+typedef struct _node
 {
-   uint8_t sfr1:2;//这里冒号后面的叫做位段,表示其占用的位数
-   uint8_t sfr2:1;
-   uint8_t sfr3:1;
-   uint8_t sfr4:4;
-}SFR;//加了位段也要保证结构体占用字节数为结构体内最大元素长度的倍数
-
+   int data;
+   struct _node *next;
+} Node;
+typedef struct _list
+{
+   Node *head;
+   Node *tail;
+} List;
+void add_Node(List *list, int number);
+void list_print(Node *p);
+int myscanf_int(int *n, int size);
 int main()
 {
-   SFR r;
-   r.sfr1=2;
-   r.sfr2=1;
-   r.sfr3=0;
-   r.sfr4=4;
-   unsigned char mask=1u<<7;//1000 0000
-   for(;mask;mask>>=1)//只有unsigned类型最高位为1时右移补0
+   int number = 0;
+   List list1;
+   list1.head = list1.tail = NULL;
+   while (1)
    {
-      printf("%d",(*(char*)&r)&mask?1:0);//强制把结构体变量类型转化为char类型后输出其二进制
+      printf("输入数据为链表赋值，-1表示停止赋值\n");
+      myscanf_int(&number, 9);
+      if (number == -1)
+         break;
+      add_Node(&list1, number);
    }
-   printf("\n");
-   printf("sizeof(SFR)=%d\n",sizeof(SFR));
+   list_print(list1.head);
+}
+void add_Node(List *list, int number)
+{
+   //创建新节点
+   Node *newNode = (Node *)malloc(sizeof(Node));
+   newNode->data = number;
+   newNode->next = NULL;
+   //连接节点
+   if (list->tail!=NULL)//如果链表的尾部指向的不是NULL则将新节点连接到tail后面
+   {
+      list->tail->next = newNode;
+      list->tail = list->tail->next;
+   }
+   else //如果链表的尾部指向的是NULL，则将头部和尾部都连接到新节点
+      list->tail = list->head = newNode;
+}
+void list_print(Node *p)
+{
+   while (p != NULL)
+   {
+      printf("%d ", p->data);
+      p = p->next;
+   }
+}
+int myscanf_int(int *n, int size)
+{
+   if (size > 0)
+      size++;
+   else
+      printf("size应为正整数\n");
+   char str[size];
+   fgets(str, size, stdin);
+   if (str[strcspn(str, "\n")] == '\n')
+      str[strcspn(str, "\n")] == '\0';
+   else
+      while (getchar() != '\n')
+         ;
+   *n = atoi(str);
 }
