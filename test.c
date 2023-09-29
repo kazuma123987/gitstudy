27f990c24be5e6ae9ae7e1d63867a078ff7fd029
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-29 15:43:20 +0800

    无回显输入密码实现方法

--- test.c
+++ test.c
@@ -2,21 +2,35 @@
 #include "stdlib.h"
 #include "string.h"
 #include "stdint.h"
+#include "conio.h"
+#define password_size 20
+void input_key(char key[])
+{
+   char ch=0;
+   unsigned int i=0;
+   while((ch=_getch())!='\r')//_getch是windows API中conio.h中的函数，是getch的优化(优化后支持更多编译器)
+   //相比于getchar,_getch不会在屏幕上显示stdin流的信息，称为无回显输入,注意只有windows平台上能使用
+   {
+      if(ch=='\b'&&i>0)
+      {
+         key[--i]='\0';//删除上一次输入的信息
+         putchar('\b');//光标退格
+         putchar(' ');//空格删除上一次输出的信息，之后光标到下一格
+         putchar('\b');//光标退格
+      }
+      else if(ch>='0'&&ch<='9'&&i<password_size)//仅允许输入0-9或回车键，且密码长度满了后无法输入
+      {
+         key[i++]=ch;
+         putchar('*');
+      }
+   }
+   key[i]='\0';
+}
 int main()
 {
-   int len=10;//定义要发送的数据长度
-   char data[]="1010010110111100";//要发送的数据，实际长度可能比定义的长
-   printf("data[1]=%d\n",data[1]);//在data[17]中，data[0]='1'=49,data[1]='0'=48,data[16]='\0'=0
-   printf("-------------------分割线-------------------\n");
-   /*-------------------%10s和%.10s-------------------*/
-   printf("%10s\n",data);//虽小于data长度,但输出完整17字节数据
-   printf("%20s\n",data);//大于data长度,输出完整17字节数据，多余的在左边用空格填充
-   printf("%.10s\n",data);//小于data长度，只输出10字节数据
-   printf("%.20s\n",data);//虽大于data长度,但只输出完整17字节数据
-   printf("-------------------分割线-------------------\n");
-   /*-------------------%*s和%.*s-------------------*/
-   printf("%*s\n",len,data);//%10s
-   printf("%*s\n",20,data);//%20s
-   printf("%.*s\n",len,data);//%.10s
-   printf("%.*s\n",20,data);//%.20s
+   char key[password_size+1];//保证密码长度能达到20
+   printf("input the key,press 'enter' to exit\npassword:");
+   input_key(key);
+   printf("\npassword:%s",key);
+   return 0;
 }
