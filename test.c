f08d91128f0c377838173f53393b70ee70749334
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-09-30 20:27:13 +0800

    队列的链表实现以及字符串的返回方式

--- test.c
+++ test.c
@@ -1,72 +1,94 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <limits.h>
-//默认为顺环队列
+#include <stdlib.h>//malloc,free,rand,srand,system,exit,atoi,itoa,abs,labs,qsort
+#include <stdbool.h>//bool,true,false
+#include <limits.h>//INT_MAX
+#include <string.h>//memcpy,strcat...
+#define data_size 4
+//定义链表节点
+typedef struct _node
+{
+   char data[data_size];
+   struct _node *next; 
+}Node;
+//定义队列
 typedef struct _quene
 {
-   int front;//队首
-   int rear;//队尾+1，无数据
-   int queCapacity;//队列大小，实际能放的元素为大小-1
-   int data[0];//队列元素
-}arrey_quene;
-//创造队列
-arrey_quene *create_quene(int capacity)
+   int size;
+   Node *front;
+   Node *rear;
+}linkedlist_quene;
+//创建新队列
+linkedlist_quene *create_quene()
 {
-   arrey_quene *q=malloc(sizeof(arrey_quene)+capacity);//实际能放的元素为大小-1
-   q->front=q->rear=0;
-   q->queCapacity=capacity;
+   linkedlist_quene *q=malloc(sizeof(linkedlist_quene));
+   q->front=q->rear=NULL;
+   q->size=0;
    return q;
 }
-//删除队列
-void free_quene(arrey_quene **q)
+//清除队列
+void free_quene(linkedlist_quene **q)
 {
+   for(Node *cur=(*q)->front,*del=NULL;cur;del=cur,cur=cur->next)
+   {
+      free(del);
+   }
    free(*q);
    *q=NULL;
 }
 //入队
-void push_quene(arrey_quene *q,int data)
+void push_quene(linkedlist_quene *q,char *data)
 {
-   if((q->rear+1)%(q->queCapacity)==q->front)printf("队列已满\n");
-   else 
+   Node *newNode=malloc(sizeof(Node));
+   memcpy(newNode->data,data,data_size-1);
+   newNode->data[data_size-1]='\0';
+   newNode->next=NULL;
+   if(q->front==NULL)q->front=q->rear=newNode;
+   else
    {
-      q->data[q->rear]=data;
-      q->rear=(q->rear+1)%q->queCapacity;
+      q->rear->next=newNode;
+      q->rear=newNode;
    }
+   q->size++;
 }
 //出队
-int pop_quene(arrey_quene *q)
+char *pop_quene(linkedlist_quene *q)
 {
-   if(q->rear==q->front)
+   static char ret[data_size]={0};//static调用的变量全局存在
+   Node *node=q->front;
+   if(q->front==NULL)//队列为空
    {
-      printf("队列已空");
-      return INT_MAX;
+      printf("队列已空\n");
+      return NULL;
    }
-   else 
+   else if(q->front==q->rear)//队列只有一个节点
    {
-      int ret=q->data[q->front];
-      q->front=(q->front+1)%q->queCapacity;
-      return ret;
+      memcpy(ret,node->data,data_size-1);
+      q->front=q->rear=NULL;
    }
+   else//队列不只有一个节点
+   {
+      memcpy(ret,node->data,data_size-1);
+      q->front=q->front->next;
+   }
+   q->size--;//出队后队列长度-1
+   free(node);//清除出队的节点
+   return ret;//返回静态局部变量的地址
 }
-//判断队列是否为空
-bool isEmpty(arrey_quene *q)
+//返回队列的长度
+int size_quene(linkedlist_quene *s)
 {
-   return q->front==q->rear;
+   return s->size;
 }
-//返回队列最大容量
-int quene_cap(arrey_quene *q)
+int main()
 {
-   return (q->queCapacity-1);
-}
-int main(int argc,char *args[])
-{
-   arrey_quene *q=create_quene(4);
-   push_quene(q,1);
-   push_quene(q,2);
-   push_quene(q,3);
-   push_quene(q,4);
-   while(!isEmpty(q))printf("%d ",pop_quene(q));
+   linkedlist_quene *q=create_quene();
+   push_quene(q,"12345");
+   push_quene(q,"23456");
+   push_quene(q,"34567");
+   printf("%s\n",pop_quene(q));
+   printf("%s\n",pop_quene(q));
+   printf("%s\n",pop_quene(q));
+   printf("%s\n",pop_quene(q));
    free_quene(&q);
-   return 0;
 }
+
