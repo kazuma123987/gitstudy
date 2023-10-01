f3d2125e80d75a6c782b1657ff2573708c3d144d
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-01 22:59:58 +0800

    指针数组的理解和使用

--- test.c
+++ test.c
@@ -1,149 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>//malloc,free,rand,srand,system,exit,atoi,itoa,abs,labs,qsort
-#include <stdbool.h>//bool,true,false
-#include <limits.h>//INT_MAX
-#include <string.h>//memcpy,strcat...
-//定义节点
-typedef struct _node
-{
-   int data;
-   struct _node *pre;//前一个节点
-   struct _node *next;//后一个节点
-}Node;
-//定义双向队列
-typedef struct _deque
-{
-   int size;
-   Node *front;
-   Node *rear;
-}deque;
-//创建双向队列
-deque *create_deque()
-{
-   deque *q=malloc(sizeof(deque));
-   q->front=q->rear=NULL;
-   q->size=0;
-}
-//清除双向队列空间
-void free_deque(deque **q)
-{
-   for(Node *cur=(*q)->front,*del=NULL;cur;del=cur,cur=cur->next)
-      free(del);
-   free(*q);
-   *q=NULL;
-}
-//队首入队
-void push_front(deque *q,int data)
-{
-   //创建新节点
-   Node *newNode=malloc(sizeof(Node));
-   newNode->data=data;
-   newNode->pre=NULL;
-   //连接节点
-   if(q->front==NULL)
-   {
-      newNode->next=NULL;
-      q->front=q->rear=newNode;
-      q->size++;
-      return;
-   }
-   q->front->pre=newNode;
-   newNode->next=q->front;
-   q->front=newNode;
-   //更新队列长度
-   q->size++;
-}
-//队尾入队
-void push_rear(deque *q,int data)
-{
-   //创建新节点
-   Node *newNode=malloc(sizeof(Node));
-   newNode->data=data;
-   newNode->next=NULL;
-   //连接节点
-   if(q->front==NULL)
-   {
-      newNode->next=NULL;
-      q->front=q->rear=newNode;
-      q->size++;
-      return;
-   }
-   q->rear->next=newNode;
-   newNode->pre=q->rear;
-   q->rear=newNode;
-   //更新队列长度
-   q->size++;
-}
-//队首出队
-int pop_front(deque *q)
-{
-   int ret=-1;
-   Node *del=q->front;
-   if(q->size==0)
-   {
-      printf("双向队列已空\n");
-      return INT_MAX;//直接退出，防止对q->size造成干扰
-   }
-   else if(q->size==1)
-   {
-      ret=q->front->data;
-      q->front=q->rear=NULL;
-   }
-   else
-   {
-   ret=q->front->data;
-   q->front=q->front->next;
-   q->front->pre=NULL;
-   }
-   free(del);
-   q->size--;//别忘记更新队列长度
-   return ret;
-}
-//队尾出队
-int pop_rear(deque *q)
-{
-   int ret=-1;
-   Node *del=q->rear;
-   if(q->size==0)
-   {
-      printf("双向队列已空\n");
-      return INT_MAX;//直接退出，防止对q->size造成干扰
-   }
-   else if(q->size==1)
-   {
-      ret=q->rear->data;
-      q->front=q->rear=NULL;
-   }
-   else
-   {
-      ret=q->rear->data;
-      q->rear=q->rear->pre;
-      q->rear->next=NULL;
-   }
-   free(del);
-   q->size--;//别忘记更新队列长度
-   return ret;
-}
-//返回双向队列尺寸
-int size_deque(deque *q)
-{
-   return q->size;
-}
-//判断双向队列是否为空
-bool isEmpty(deque *q)
-{
-   return q->size==0;
-}
+#include <stdlib.h>  //malloc,free,rand,srand,system,exit,atoi,itoa,abs,labs,qsort
+#include <stdbool.h> //bool,true,false
+#include <limits.h>  //INT_MAX
+#include <string.h>  //memcpy,strcat...
 int main()
 {
-   deque *q=create_deque();
-   push_front(q,1);
-   push_front(q,2);
-   push_rear(q,3);
-   printf("%d ",pop_rear(q));
-   printf("%d ",pop_rear(q));
-   printf("%d ",pop_rear(q));
-   push_rear(q,4);
-   printf("%d ",pop_front(q));
-   free_deque(&q);
+   int *p[3];//定义了一个指针数组，可看作int *p0,*p1,*p2；定义后p[0],p[1],p[2]就是三个未分配空间的指针
+   /*-------------------场景1（数字地址赋值）-------------------*/
+   int a=1,b=2,c=3;
+   //定义以后赋值
+   p[0]=&a;
+   p[1]=&b;
+   p[2]=&c;
+   //定义时赋值
+   int *x[3]={&a,&b,&c};//*x[0]=1,*x[1]=2,*x[2]=3
+   printf("*p[1]=%d,*x[1]=%d\n",*p[1],*x[1]);
+   /*-------------------场景2（字符串地址赋值）-------------------*/
+   char *str1="hello";
+   char *str2=",";
+   char *str3="world!";
+   char *str[3]={str1,str2,str3};
+   printf("%s%s%s\n",str[0],str[1],str[2]);
+   /*-------------------场景3(动态内存分配自己赋值)-------------------*/
+   for(int i=0;i<3;i++)
+   {
+   p[i]=malloc(sizeof(int));
+   *p[i]=i+1;
+   printf("%d ",*p[i]);
+   }
 }
