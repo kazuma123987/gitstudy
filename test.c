07ec8325a7f47595c62d7e809d4efce01ac99937
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-02 22:29:02 +0800

    memset的使用及内存相关

--- test.c
+++ test.c
@@ -3,142 +3,21 @@
 #include <stdbool.h> //bool,true,false
 #include <limits.h>  //INT_MAX
 #include <string.h>  //memcpy,strcat...
-#define MAX_BUCKECTS_SIZE 100
-//键值对变量
-typedef struct{
-   int key;
-   char *val;
-}pair;
-//哈希表结构体
-typedef struct{
-   pair *buckets[MAX_BUCKECTS_SIZE];
-}hashMap;
-//有效键值对容器(包含个数)
-typedef struct{
-   void *sets;
-   int size;
-}mapCap;
-//创建哈希表
-hashMap *create_hashmap()
-{
-   hashMap *map=malloc(sizeof(hashMap));
-   for(int i=0;i<MAX_BUCKECTS_SIZE;i++)
-   map->buckets[i]=NULL;
-   return map;
-}
-//清除哈希表
-void free_hashmap(hashMap **map)
-{
-   for(int i=0;i<MAX_BUCKECTS_SIZE;i++)
-      free((*map)->buckets[i]);
-   free(*map);
-   *map=NULL;
-}
-//哈希函数
-int hash_function(int key)
-{
-   return key%MAX_BUCKECTS_SIZE;
-}
-//添加键值对
-void putItem(hashMap *map,const int key,const char *val)
-{
-   pair *p=malloc(sizeof(pair));
-   p->key=key;
-   p->val=malloc(strlen(val)+1);//注意这里不能用sizeof，否则得到的是指针的长度8（64位）
-   strcpy(p->val,val);
-   int index=hash_function(key);
-   map->buckets[index]=p;
-}
-//删除键值对
-void removeItem(hashMap *map,const int key)
-{
-   int index=hash_function(key);
-   free(map->buckets[index]);
-   map->buckets[index]=NULL;
-}
-//获取有效键值对
-mapCap *map_get(hashMap *map)
-{
-   mapCap *mapcap=malloc(sizeof(mapCap));
-   int size=0;
-   for(int i=0;i<MAX_BUCKECTS_SIZE;i++)
-   {
-      if(map->buckets[i]!=NULL)size++;
-   }
-   pair *p=malloc(sizeof(pair)*size);
-   for(int i=0,index=0;i<MAX_BUCKECTS_SIZE;i++)
-   {
-      if(map->buckets[i]!=NULL)
-      {
-         p[index].key=map->buckets[i]->key;
-         p[index].val=malloc(strlen(map->buckets[i]->val)+1);
-         strcpy(p[index].val,map->buckets[i]->val);
-         index++;
-      }
-   }
-   mapcap->sets=p;
-   mapcap->size=size;
-}
-//获取所有键
-mapCap *key_get(hashMap *map)
-{
-   mapCap *cap=malloc(sizeof(mapCap));
-   int size=0;
-   for(int i=0;i<MAX_BUCKECTS_SIZE;i++)if(map->buckets[i]!=NULL)size++;
-   int *key=malloc(sizeof(int)*size);
-   for(int i=0,index=0;i<MAX_BUCKECTS_SIZE;i++)
-   {
-      if(map->buckets[i]!=NULL)
-         key[index++]=map->buckets[i]->key;
-   }
-   cap->sets=key;
-   cap->size=size;
-   return cap;
-}
-//获取所有值
-mapCap *val_get(hashMap *map)
-{
-   mapCap *cap=malloc(sizeof(mapCap));
-   int size=0;
-   for(int i=0;i<MAX_BUCKECTS_SIZE;i++)if(map->buckets[i]!=NULL)size++;
-   char **p=malloc(sizeof(char *)*size);
-   for(int i=0,index=0;i<MAX_BUCKECTS_SIZE;i++)
-      if(map->buckets[i]!=NULL)
-      {
-         p[index]=malloc(strlen(map->buckets[i]->val)+1);
-         p[index]=map->buckets[i]->val;
-         index++;
-      }
-   cap->sets=p;
-   cap->size=size;
-   return cap;
-}
-int main()
-{
-   hashMap *map=create_hashmap();
-   //添加键值对
-   putItem(map,11232,"小红");
-   putItem(map,11367,"小华");
-   putItem(map,11221,"小明");
-   //获取全部有效键值对
-   mapCap *cap1=map_get(map);
-   pair *p1=(pair *)cap1->sets;
-   printf("%s\n",p1[0].val);
-   free(cap1->sets);
-   //获取所有有效值
-   mapCap *cap2=key_get(map);
-   int *p2=(int *)cap2->sets;
-   printf("%d\n",p2[1]);
-   removeItem(map,11232);//移除元素
-   free(cap2->sets);
-   //更新移除元素后的键值对
-   mapCap *cap3=map_get(map);
-   free(cap3->sets);
-   //获取所有键
-   mapCap *cap4=val_get(map);
-   char **p=(char **)cap4->sets;
-   printf("%s\n",p[1]);
-   free(cap4->sets);
-   free_hashmap(&map);
-   return 0;
+//memset(*ptr,int c,size_t size);从指针ptr指向的（值）所存放的内存空间开始,以（字节）为单位，将连续size个字节赋值为c
+int main() {
+    int *p[5];//创建指针数组(*p)[5]
+    int *arr=malloc(sizeof(int)*10);//创建整数数组arr[10]
+    char str[]="hello world",c=49;//创建字符数组str[sizeof("hello world")]
+    
+    memset(p, 0, sizeof(int*) * 5);//从p所指的第一个元素(int *)p[0]开始,将连续5*sizeof(int*)个字节的值设置为0，最终所有指针被设置为0
+    memset(arr,1,sizeof(int)*10);//从arr所指的第一个元素(int)arr[0]开始,将连续5*sizeof(int)个字节的值设置为1，如下图:
+    //比如arr[0]        arr[1]       arr[2]       arr[3] ...   arr[9]
+    //  0x01010101   0x01010101    0x01010101   0x01010101    0x01010101 (十六进制按int大小排序)
+    //  01 01 01 01  01 01 01 01  01 01 01 01  01 01 01 01    01 01 01 01(十六进制按字节排序)
+    //  16843009       16843009     16843009     16843009     16843009   (十进制按int大小排序)
+    memset(str,c,strlen(str));//从str所指的值(str[0]='h')开始,以字节为单位,将连续strlen个字节的值设置为49(字符'1')
+    for (int i = 0; i < 5; i++)printf("p[%d]=%p\n",i, p[i]);//打印p指针数组的元素,因为是指针，所以有8个字节，16位16进制表示
+    for (int i = 0; i < 10; i++)printf("%d ",arr[i]);//打印arr整数数组的元素
+    printf("\n%s",str);
+    return 0;
 }
