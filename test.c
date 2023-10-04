ab7a1710072663393b33f0dbe11195230a5bbcf0
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-04 22:49:47 +0800

    使用realloc给动态数组扩容

--- test.c
+++ test.c
@@ -4,21 +4,23 @@
 #include <limits.h>  //INT_MAX
 #include <string.h>  //memcpy,strcat...
 #include <windows.h> //Sleep()
-#include <conio.h>  //kbhit(),_getch()
-/*--------------------------移位操作要对unsigned类型做，因为非unsigned右移时符号位不变--------------------------*/
+#include <conio.h>   //kbhit(),_getch()
+typedef struct _tree
+{
+    int value;
+    int height;
+    struct _tree *left;
+    struct _tree *right;
+} Tree;
 int main()
 {
-    unsigned int a=0x12345678;
-    /*分析，a左移4位后后四位填0，右移28位后左边28位为0，与0相异或就是本身，最终实现了位的左旋转操作*/
-    unsigned int b=(a<<4)^(a>>(sizeof(int)*8-4));//二进制左移四位就是十六进制左移1位
-    unsigned char t=0x01;//这里t可代表8位led灯
-    while(!kbhit())//如果没有按键输入则一直循环
-    {
-        t=(t<<1)^(t>>7);//每隔1000ms把t左移一位
-        for(unsigned char mask=1<<7;mask;mask>>=1)
-            printf("%d",mask&t?1:0);
-        printf("\n");
-        Sleep(1000);
-    }
-    printf("%x",b);
+    int len=3;//数组长度
+    int *arr = malloc(sizeof(int) * len);//创建数组arr[3]
+    for(int i=0;i<len;i++)arr[i]=i+1;
+    int len_new=len+3;//扩容后的数组长度
+    arr = realloc(arr, sizeof(int) * len_new);//扩容arr[3]到arr[6],数组内容不变
+    for(int i=len;i<len_new;i++)arr[i]=i+1;
+    for(int i=0;i<len_new;i++)//打印数组内容
+        printf("%d ", arr[i]);
+    free(arr);
 }
