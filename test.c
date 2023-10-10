f9242e73273d8e021d9ac41667a66e4f550513b4
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-10 22:59:12 +0800

    平衡二叉树的代码实现以及递归删除和插入的使用

--- test.c
+++ test.c
@@ -5,173 +5,201 @@
 #include <string.h>  //memcpy,strcat...
 #include <windows.h> //Sleep()
 #include <conio.h>   //kbhit(),_getch()
+// 树节点
 typedef struct _tree
 {
     int val;
     int height;
     struct _tree *left;
     struct _tree *right;
-} Tree;
-// 创建树
-Tree *create_tree(int val)
+} TreeNode;
+// AVL树封装
+typedef struct
 {
-    Tree *t = malloc(sizeof(Tree));
-    t->val = val;
-    t->height = 1;
-    t->left = t->right = NULL;
-    return t;
+    TreeNode *root;
+} AVL_Tree;
+// 创建新节点
+TreeNode *newnode(int val)
+{
+    TreeNode *node = malloc(sizeof(TreeNode));
+    node->val = val;
+    node->left = node->right =NULL;
+    node->height = 1;
+    return node;
 }
-// 清除树
-void free_tree(Tree *t)
+// 创建新树
+AVL_Tree *newAVL()
+{
+    AVL_Tree *avl = malloc(sizeof(AVL_Tree));
+    avl->root = NULL;
+    return avl;
+}
+// 返回节点高度
+int height(TreeNode *t)
+{
+    if (t != NULL)
+        return t->height;
+    return 0;
+}
+// 更新节点高度
+void height_update(TreeNode *t)
 {
     if (t == NULL)
         return;
-    free_tree(t->left);
-    free_tree(t->right);
-    free(t);
+    int lh = height(t->left);
+    int rh = height(t->right);
+    if (lh >= rh)
+        t->height=lh + 1;
+    else
+        t->height=rh + 1;
 }
-// 搜索节点
-Tree *search_tree(Tree *root, int val)
+// 平衡因子
+int balanceFactor(TreeNode *t)
 {
-    Tree *t = root;
-    while (t != NULL)
-    {
-        if (t->val > val)
-            t = t->left;
-        else if (t->val < val)
-            t->right;
-        else
-            return t;
-    }
-    printf("没找到节点！");
-    return NULL;
+    if (t == NULL)
+        return 0;
+    return height(t->left) - height(t->right);
 }
-// 插入节点
-void insert_tree(Tree *root, int val)
+// 左旋转(只会在旋转操作函数失衡下调用，所以不用考虑其他情况)
+TreeNode *lRotate(TreeNode *t)//左旋一般以child节点为中心，对节点t逆时针旋转到child的left端，
+//但如果child的left端有节点grandchild，则将grandchild放在t的右端（因为child和grandchild都在t的右边，比t大）
 {
-    if (root == NULL)return;//如果根节点为NULL则直接返回
-    Tree *node = root;//创建一个指针指向根节点
-    while (node->left != NULL || node->right != NULL)//如果有左节点或者右节点就要循环
-    {
-        if (node->val > val)//如果要插入的值大于当前节点则往左子树找，如果左子树是空则直接跳出循环
-        {
-            if(node->left)node = node->left;
-            else break;
-        }
-        else if (node->val < val)//如果要插入的值小于当前节点则往右子树找，如果右子树是空则直接跳出循环
-        {
-            if(node->right)node = node->right;
-            else break;
-        }
-        else if (node->val == val)//如果要插入的值和当前节点相同则跳出函数
-            return;
-    }
-    if (node->val > val)//插入左节点
-        node->left = create_tree(val);
-    else if (node->val < val)//插入右节点
-        node->right = create_tree(val);
+    TreeNode *child = t->right;//左旋说明右方倾斜，则子节点朝右
+    TreeNode *grandchild = child->left;
+    child->left = t;
+    t->right = grandchild;
+    height_update(t);
+    height_update(child);
+    return child;
 }
-// 删除节点
-void delete_tree(Tree *root, int val)
+// 右旋转(同上，不会出现错误调用情况)
+TreeNode *rRotate(TreeNode *t)
 {
-    int flag = -1;//建立一个标志，flag为0表示删除节点在父节点的左子节点，为1则反之
-    Tree *cur = root, *pre = NULL;//定义一个cur和pre，cur代表待删除节点，pre代表待删除节点的父节点
-    while (cur && cur->val != val)//通过循环找到待删除节点，将cur和pre指向它们代表的节点，并更新flag的值
+    TreeNode *child = t->left;
+    TreeNode *grandchild = child->right;
+    child->right = t;
+    t->left = grandchild;
+    height_update(t);
+    height_update(child);
+    return child;
+}
+// 旋转操作
+TreeNode *rotate(TreeNode *t)
+{
+    int factor = balanceFactor(t);
+    if (factor > 1)//左偏
     {
-        if (cur->val > val)//如果val值大于当前节点，则往右子树找
+        if (balanceFactor(t->left) >= 0)//子节点也左偏，右转
+            return rRotate(t);
+        else//子节点右偏，左右旋转（先子节点左转，再父节点右转）
         {
-            pre = cur;
-            cur = cur->left;
-            flag = 0;
-        }
-        else//如果val值小于当前节点，则往左子树找
-        {
-            pre = cur;
-            cur = cur->right;
-            flag = 1;
+            t->left = lRotate(t->left);
+            return rRotate(t);
         }
     }
-    if (cur != NULL)//如果cur不为NULL(找到了要删除的节点)
+    else if (factor < -1)//右偏
     {
-        if (root->val == val)//如果要删根节点，直接free掉
-            free(root);
-        else if (cur->left == NULL && cur->right == NULL)//如果要删叶节点，直接free掉并使父节点指向叶节点改为指向NULL
+        if (balanceFactor(t->right) <= 0)//子节点也右偏，左转
+            return lRotate(t);
+        else//子节点左偏，右左旋转（先子节点右转，再父节点左转）
         {
-            free(cur);
-            if (flag)
-                pre->right = NULL;
-            else
-                pre->left = NULL;
-        }
-        else if (cur->left && !cur->right)//如果要删节点只有一个左子树，将父节点指向左子树后free掉待删除节点
-        {
-            if (flag)
-                pre->right = cur->left;
-            else
-                pre->left = cur->left;
-            free(cur);
-        }
-        else if (!cur->left && cur->right)//如果要删节点只有一个右子树，将父节点指向右子树后free掉待删除节点
-        {
-            if (flag)
-                pre->right = cur->right;
-            else
-                pre->left = cur->right;
-            free(cur);
-        }
-        else//如果要删节点又有左子树又有右子树，那么将右子树最小节点代替待删除节点后删除节点
-        {
-            Tree *t = cur->right, *p = NULL;//先用t指向右子树根节点，t之后代表右子树最小节点，p之后代表t的父节点
-            while (t->left)//通过循环使t和p符合其定位
-            {
-                p = t;
-                t = t->left;
-                if (t->left == NULL)//循环结束时t指向最小节点
-                    break;
-            }
-            if (flag)//父节点先指向待删节点右子树最小节点
-                pre->right = t;
-            else
-                pre->left = t;
-            t->left = cur->left;//最小节点左边指向待删节点左子树
-            if (p)//如果p不是NULL(最小节点不是右子树根节点),则把最小节点右边再指向待删节点右边(右子树根节点)，并把原父节点的左边指向NULL
-            {
-                t->right = cur->right;
-                p->left = NULL;
-            }
-            free(cur);//删除待删节点
+            t->right = rRotate(t->right);
+            return lRotate(t);
         }
     }
-    else printf("未找到要删除节点");
+    else//不偏则直接返回原值
+        return t;
 }
-Tree *arrTotree(int *arr, int size)
+//递归插入操作
+TreeNode *insert(TreeNode *t, int val)
 {
-    if (size <= 0)
-        return NULL;
-    Tree *t = create_tree(arr[0]);
-    t->left = t->right = NULL;
-    t->height = 1;
-    for (int i = 1; i < size; i++)
-        insert_tree(t, arr[i]);
+    if (t == NULL)//如果找到了尽头，则给父节点连接新的子节点
+        return newnode(val);
+    if (t->val > val)//要插入的值更小，则往左边插入
+        t->left = insert(t->left, val);
+    else if (t->val < val)//要插入的值更大，则往右边插入
+        t->right = insert(t->right, val);
+    else//如果有值和插入的值相等，则不插入，直接返回原值
+        return t;
+    t = rotate(t);//旋转操作
+    height_update(t);//因为当插入后如果没偏那rotate会直接返回，不会更新高度，所以在这里要更新高度
     return t;
 }
-void inPrint(Tree *t)
+//递归删除操作
+TreeNode *removeItem(TreeNode *t, int val)//递归删除时一条路径上的节点都应旋转并更新高度
+{
+    if (t == NULL)
+        return t;
+    if (t->val > val)
+        t->left = removeItem(t->left, val);//传递旋转及更新后的左节点
+    else if (t->val < val)
+        t->right = removeItem(t->right, val);//传递旋转及更新后的右节点
+    else//找到了要删除的节点
+    {
+        if (t->left == NULL || t->right == NULL)
+        {
+            TreeNode *node = t->left;
+            if (t->right != NULL)
+                node = t->right;
+            free(t);
+            if (node == NULL)//如果t是叶节点，则删除后直接返回NULL传递给父节点
+                return NULL;
+            else//如果t有一个子节点（但子节点可能还有子节点），所以删除后赋值node给t，
+            //旋转并更新t的高度后把t传递给父节点（如果没错误操作其实可以直接return node）
+                t = node;
+        }
+        else//如果待删除的节点t有两个子节点
+        {
+            TreeNode *cur;
+            for (cur=t->right;cur->left;cur = cur->left);//找到t的右子树的最小节点，赋值给cur
+            int cur_val=cur->val;//暂存cur的值
+            t->right=removeItem(t->right,cur_val);//删除t的右子树的最小节点后把根节点传递给t的右指针
+            t->val=cur_val;//把t的值替换为最小节点的值（这个操作相当于删除了t，并把右子树最小节点替换t的位置）
+        }
+    }
+    t=rotate(t);//在路径上递归旋转节点的值
+    height_update(t);//更新高度，防止rotate直接返回时未更新
+    return t;//传递更新后的值给路径上的父节点
+}
+//查找操作
+TreeNode *find(TreeNode *t,int val)
+{
+    if(t==NULL)return NULL;
+    if(t->val>val)return find(t->left,val);
+    else if(t->val<val)return find(t->right,val);
+    else return t;
+}
+//中序遍历打印树
+void printAVL(TreeNode *root)
+{
+    if(root==NULL)return;
+    printAVL(root->left);
+    printf("%d ",root->val);
+    printAVL(root->right);
+}
+//递归清除树
+void freeTree(TreeNode *t)
 {
     if(t==NULL)return;
-    inPrint(t->left);
-    printf("%d ",t->val);
-    inPrint(t->right);
+    freeTree(t->left);
+    freeTree(t->right);
+    free(t);
+}
+//清除AVL树
+void freeAVL(AVL_Tree *avl)
+{
+    freeTree(avl->root);
+    free(avl);
 }
 int main()
 {
-    int arr[]={0,1,3,4,14,12,5,2,7,8,10};
-    Tree *t=arrTotree(arr,sizeof(arr)/sizeof(int));
-    inPrint(t);
-    delete_tree(t,33);
-    delete_tree(t,12);
-    delete_tree(t,14);
+    AVL_Tree *avl=newAVL();
+    for(int i=1;i<11;i++)
+    avl->root=insert(avl->root,i);
+    printAVL(avl->root);
+    avl->root=removeItem(avl->root,6);
+    TreeNode *a=find(avl->root,8);
     printf("\n");
-    inPrint(t);
-    free_tree(t);
-    return 0;
+    printAVL(avl->root);
+    freeAVL(avl);
 }
