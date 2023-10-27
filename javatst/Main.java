4b067bd11adfb679911aea56e2308270c95d74ad
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-27 16:44:06 +0800

    java正则表达式

--- javatst/Main.java
+++ javatst/Main.java
@@ -1,34 +1,28 @@
 package javatst;
 
 import java.util.*;
-
-import java.time.LocalDate;//年月日
-import java.time.LocalTime;//时分秒
-import java.time.LocalDateTime;//LocalDate+LocalTime
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static boolean flag=false;
     public static void main(String[] args) {
-        Timer timer1 = new Timer();// 定时器1
-        Timer timer2 = new Timer();// 定时器2
-        TimerTask task1 = new TimerTask() // 定时任务
-        {
-            @Override
-            public void run() {
-                System.out.println(LocalDateTime.now().toString());
-            }
-        };
-        TimerTask task2 = new TimerTask() {
-            @Override
-            public void run() {
-              flag=true;
-              timer1.cancel();
-              timer2.cancel();  
-            }
-        };
-        while (!flag) {
-            timer1.schedule(task1, 0, 100);
-            timer2.schedule(task2, 1000);
+        String text1 = "3.1441415.15155.515.345";
+        String text2= " 你好 世界\n";
+        //^表示从哪里开始,如果不加^,Matcher.find()方法会持续搜寻相对应的字符串(可以尝试去掉^)，\d匹配一个数字
+        //？表示前面括号的内容是可以选的，可以匹配\\d+(\\.\\d+)或\\d+
+        //这里\\d+表示匹配可匹配多位数
+        String regex1 = "^\\d+(\\.\\d+)?";
+        String regex2="世界";//字符串只会输出"世界"
+        Pattern pattern1 = Pattern.compile(regex1);
+        Pattern pattern2 = Pattern.compile(regex2);
+        Matcher matcher1 = pattern1.matcher(text1);
+        Matcher matcher2 = pattern2.matcher(text2);
+        
+        while (matcher1.find()) {
+            System.out.println("Match: " + text1.substring(matcher1.start(), matcher1.end()));//start()和end()方法传的是字符串的序号
+        }
+        while (matcher2.find()) {
+            System.out.println("Match: "+matcher2.group());//group()方法可以实现上述substring()的结果
         }
     }
 }
