a3ec7084e83aa9f5df867c15b5fdef4bbea52bc0
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-10-28 21:28:02 +0800

    fmod第三方库和opengl窗口的创建

---
+++ test.h
@@ -0,0 +1,14 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+#include <stdio.h>
+#include <stdlib.h>  //malloc,free,rand,srand,system,exit,atoi,itoa,abs,labs,qsort
+#include <stdbool.h> //bool,true,false
+#include <limits.h>  //INT_MAX
+#include <string.h>  //memcpy,strcat...
+#include <stdint.h>  //uint16_t,int8_t..B.
+#include <windows.h> //Sleep()
+#include <conio.h>   //kbhit(),_getch()
+#include <time.h>    //time(),time_t,ctime
+#include <stdint.h>
+#include <math.h>
+#endif
