a94cb900930515d300facc511d6d5e1d53a9f203
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-11-21 19:37:25 +0800

    材质贴图的使用与放射光

--- glLearn/include/camera.h
+++ glLearn/include/camera.h
@@ -6,10 +6,15 @@ class Camera
 {
 public:
 	Camera();
+	//更新观察矩阵、投影矩阵和相机移动速度
 	void update();
+	//把观察矩阵和投影矩阵传到uniform变量
 	void updateMat(Shader shader);
+	//键盘WASD控制相机位置向量
 	void keyboardInput(GLFWwindow* window);
+	//鼠标移动位置控制欧拉角大小
 	void curseInput(GLFWwindow* window);
+	//鼠标滚轮控制相机投影矩阵的角度fov(影响视野)
 	void scrollCallback(float offset);
 	glm::vec3 getCameraPos();
 	glm::mat4 getViewMat();
@@ -28,5 +33,4 @@ private:
 	glm::mat4 view;//观察矩阵
 	glm::mat4 proj;//透视投影矩阵
 };
-
 #endif // !_CAMERA_H_
