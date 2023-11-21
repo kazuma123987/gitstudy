d0edaf03ffd7f450aa56dcc67dc0ee8b1bf5081f
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2023-11-21 19:37:25 +0800

    材质贴图的使用与放射光

--- glLearn/main.cpp
+++ glLearn/main.cpp
@@ -4,22 +4,23 @@
 #include "global.h"
 // N卡使用独显运行
 extern "C" __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-//不弹出调试控制台(用于Release版本)
-//#pragma comment(linker, "/subsystem:\"windows\"  /entry:\"mainCRTStartup\"")
-// 函数声明
-void frame_size_callback(GLFWwindow* window, int width, int height);
-void press_close_window(GLFWwindow* window);
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-int main(int argc, char* argv[])
+// 不弹出调试控制台(用于Release版本)
+// #pragma comment(linker, "/subsystem:\"windows\"  /entry:\"mainCRTStartup\"")
+//  函数声明
+void frame_size_callback(GLFWwindow *window, int width, int height);
+void press_close_window(GLFWwindow *window);
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset);
+int main(int argc, char *argv[])
 {
 	// 获取文件当前路径
-   // strcpy_s(filePath,"C:\\Users\\34181\\source\\repos\\glstudy\\glstudy");
+	// strcpy_s(filePath,"C:\\Users\\34181\\source\\repos\\glstudy\\glstudy");
 	GetModuleFileNameA(NULL, filePath, 260);
-	// char* c = strrchr(filePath, '\\');
-	// if (c != NULL)
-	//     *(c + 1) = '\0';
-	 // glfw初始化
+	char *c = strrchr(filePath, '\\');
+	if (c != NULL)
+		*(c + 1) = '\0';
+	//printf_s("%s", filePath);
+	// glfw初始化
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // 注意设置的glfw上下文版本
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -28,7 +29,7 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 	// 创建glfw窗口
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "game", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "game", NULL, NULL);
 	if (window == NULL)
 	{
 		fputs("\nfailed to create window", stderr);
@@ -44,7 +45,6 @@ int main(int argc, char* argv[])
 		glfwTerminate();
 		return -1;
 	}
-
 	// VAO
 	GLuint modelVAO;
 	GLuint lightVAO;
@@ -58,64 +58,71 @@ int main(int argc, char* argv[])
 	glBufferData(GL_ARRAY_BUFFER, arrVertex_N, arr_vertex, GL_STATIC_DRAW);
 	// VAO的顶点指针属性设置(先发送数据到显存,再绑定VAO，再设置顶点指针)
 	glBindVertexArray(modelVAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // 顶点坐标
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0); // 顶点坐标
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // 纹理坐标
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float))); // 纹理坐标
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float))); // 法向量坐标
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float))); // 法向量坐标
 	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(lightVAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // 顶点坐标
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0); // 顶点坐标
 	glEnableVertexAttribArray(0);
 
 	// 创建着色器程序
 	SetCurrentDirectoryA(filePath);
-	Shader modelShader("res\\shader\\model.vert", "res\\shader\\model.frag");
+	Shader modelShader("res/shader/model.vert", "res/shader/model.frag");
 	Shader lightShader("res/shader/model.vert", "res/shader/light.frag");
 
 	/*--------------------TEXTURE--------------------*/
-	GLuint texture[6];//6个纹理
+	GLuint texture[6]; // 6个纹理
 	glGenTextures(6, texture);
-	int width[6], height[6], nColorChannels[6];//纹理的宽、高、颜色通道数
-	stbi_set_flip_vertically_on_load(TRUE);//开启竖直翻转
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);//设置纹理为单字节传输
-	for (int i = 0; i < 6; i++)
+	int width[6], height[6], nColorChannels[6]; // 纹理的宽、高、颜色通道数
+	stbi_set_flip_vertically_on_load(TRUE);		// 开启竖直翻转
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);		// 设置纹理为单字节传输
+	for (int i = 0; i < 3; i++)
 	{
-		glBindTexture(GL_TEXTURE_2D, texture[i]); // 绑定第一个纹理
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glBindTexture(GL_TEXTURE_2D, texture[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // 只有缩小能开启多级渐远纹理
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		// 加载并发送纹理
-		char* imagePath = (char*)calloc(20, sizeof(char));//图片路径
+		char *imagePath = (char *)calloc(30, sizeof(char)); // 图片路径
 		if (imagePath)
-			sprintf_s(imagePath, 20, "res/texture/%d.jpg", i + 1);//str的长度应大于等于目标字符串长度+1
+			sprintf_s(imagePath, 30, "res/texture/box%d.png", i + 1); // str的长度应大于等于目标字符串长度+1
 		else
 			fputs("calloc failed", stderr);
 
-		unsigned char* imageData = stbi_load(imagePath, &width[i], &height[i], &nColorChannels[i], 0);
+		unsigned char *imageData = stbi_load(imagePath, &width[i], &height[i], &nColorChannels[i], 0);
 		if (imageData)
 		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width[i], height[i], 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+			switch (nColorChannels[i])
+			{
+			case 1:
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, width[i], height[i], 0, GL_ALPHA, GL_UNSIGNED_BYTE, imageData);
+				break;
+			case 3:
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width[i], height[i], 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+				break;
+			case 4:
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width[i], height[i], 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+			}
 			glGenerateMipmap(GL_TEXTURE_2D);
 		}
 		else
 			fputs("\nfailed to load the image", stderr);
 		stbi_image_free(imageData); // 纹理已加载，删除纹理数组
-		free(imagePath);//清除图片路径缓冲区
+		free(imagePath);			// 清除图片路径缓冲区
 	}
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);//恢复纹理单行4字节传输
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4); // 恢复纹理单行4字节传输
 	// 指定纹理单元传入的位置
 	modelShader.use();
-	modelShader.unfm1i("ourTexture[0]", 0);
-	modelShader.unfm1i("ourTexture[1]", 1);
-	modelShader.unfm1i("ourTexture[2]", 2);
-	modelShader.unfm1i("ourTexture[3]", 3);
-	modelShader.unfm1i("ourTexture[4]", 4);
-	modelShader.unfm1i("ourTexture[5]", 5);
+	modelShader.unfm1i("material.diffuseTexture", 0);
+	modelShader.unfm1i("material.specularTexture", 1);
+	modelShader.unfm1i("emission",2);
 	// 绑定纹理
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		glActiveTexture(GL_TEXTURE0 + i);
 		glBindTexture(GL_TEXTURE_2D, texture[i]);
@@ -130,23 +137,23 @@ int main(int argc, char* argv[])
 	FMOD_Sound_SetMode(s1->sound, FMOD_LOOP_NORMAL);
 	music.loadMusic(s2, "res/music/choose.wav");
 	music.playMusic(s1);
-	//模型矩阵
+	// 模型矩阵
 	glm::mat4 model[10];
 	for (int i = 0; i < 10; i++)
 	{
 		model[i] = glm::translate(glm::mat4(1.0f), offsetModel[i]);
 		model[i] = glm::rotate(model[i], glm::radians(20.0f * i), glm::vec3(1.0f, i, i));
 	}
-	//光源矩阵
+	// 光源矩阵
 	glm::mat4 unitMat = glm::mat4(1.0f);
 	glm::vec3 lightPos = glm::vec3(0.0f, 0.8f, 3.0f);
 	glm::vec3 lightColor = glm::vec3(1.0f);
-	//Camera
+	// Camera
 	camera = new Camera();
-	//glEnable
+	// glEnable
 	glEnable(GL_DEPTH_TEST);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	//绑定回调函数
+	// 绑定回调函数
 	glfwSetKeyCallback(window, keyCallback);
 	glfwSetScrollCallback(window, scrollCallback);
 
@@ -164,42 +171,40 @@ int main(int argc, char* argv[])
 		float t = 2 * glfwGetTime();
 		lightPos.z = sin(t);
 		lightPos.x = cos(t);
-		//model
+		// model
 		modelShader.use();
+		clock_t start=clock();
+		float mix=abs((t/6-(int)t/6)-0.5f)*2;//此函数效率是sin(t)的10倍
+		printf("\r%ld",clock()-start);
+		modelShader.unfm1f("yOffset",t*0.5);
+		modelShader.unfm1f("mixVector",mix);
 		camera->update();
 		camera->updateMat(modelShader);
 		glBindVertexArray(modelVAO);
-		glm::vec3 lightPosition=glm::vec3(camera->getViewMat()*glm::vec4(lightPos,1.0f));
-		lightColor.x=sin(0.2f*t)*0.5f+0.5f;
-		lightColor.y=sin(0.5f*t)*0.5f+0.5f;
-		lightColor.z=sin(0.7f*t)*0.5f+0.5f;
-		glm::vec3 lightAmbient=glm::vec3(1.0f)*lightColor;
-		glm::vec3 lightDiffuse=glm::vec3(1.0f)*lightColor;
-		glm::vec3 lightSpecular=glm::vec3(1.0f)*lightColor;
+		glm::vec3 lightPosition = glm::vec3(camera->getViewMat() * glm::vec4(lightPos, 1.0f));
+		// lightColor.x = sin(0.2f * t) * 0.5f + 0.5f;
+		// lightColor.y = sin(0.5f * t) * 0.5f + 0.5f;
+		// lightColor.z = sin(0.7f * t) * 0.5f + 0.5f;
+		glm::vec3 lightAmbient = glm::vec3(0.1f) * lightColor;
+		glm::vec3 lightDiffuse = glm::vec3(0.5f) * lightColor;
+		glm::vec3 lightSpecular = glm::vec3(1.0f) * lightColor;
 		modelShader.unfvec3fv("light.position", lightPosition);
-		modelShader.unfvec3fv("light.ambient",lightAmbient);
-		modelShader.unfvec3fv("light.diffuse",lightDiffuse);
-		modelShader.unfvec3fv("light.specular",lightSpecular);
+		modelShader.unfvec3fv("light.ambient", lightAmbient);
+		modelShader.unfvec3fv("light.diffuse", lightDiffuse);
+		modelShader.unfvec3fv("light.specular", lightSpecular);
 		for (int i = 0; i < 1; i++)
 		{
 			modelShader.unfmat4fv("model", model[i]);
 			glm::mat3 normalMat = glm::mat3(glm::transpose(glm::inverse(camera->getViewMat() * model[i])));
 			modelShader.unfmat3fv("normalMat", normalMat);
-			modelShader.unfvec3fv("material.ambient",glm::vec3(0.19225f));
-			modelShader.unfvec3fv("material.diffuse",glm::vec3(0.50754f));
-			modelShader.unfvec3fv("material.specular",glm::vec3(0.508273f));
-			modelShader.unfm1f("material.shininess",0.4 * 128.0f);
-			for (int j = 0; j < 6; j++)
-			{
-				modelShader.unfm1i("texIndex", j);
-				glDrawArrays(GL_TRIANGLES, j * 6, 6);
-			}
+			modelShader.unfm1f("material.shininess", 32.0f);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
-		//light
+		// light
 		glBindVertexArray(lightVAO);
 		lightShader.use();
 		glm::mat4 lightMat = glm::translate(unitMat, lightPos);
-		lightMat = glm::scale(lightMat, glm::vec3(0.2f));//先位移再进行缩放或者旋转
+		lightMat = glm::scale(lightMat, glm::vec3(0.2f)); // 先位移再进行缩放或者旋转
 		lightShader.unfmat4fv("model", lightMat);
 		lightShader.unfvec3fv("lightColor", lightColor);
 		camera->updateMat(lightShader);
@@ -210,26 +215,26 @@ int main(int argc, char* argv[])
 
 		// EVENTS && DISPLAY
 		glfwSwapBuffers(window);
-		glfwPollEvents();//处理窗口交互事件等
+		glfwPollEvents(); // 处理窗口交互事件等
 	}
 
 	/*--------------------释放内存--------------------*/
 	glDeleteVertexArrays(1, &modelVAO); // 删除VAO绑定的一个VAO对象
-	glDeleteBuffers(1, &modelVBO);      // 删除VBO绑定的一个缓存对象
+	glDeleteBuffers(1, &modelVBO);		// 删除VBO绑定的一个缓存对象
 	glDeleteTextures(6, texture);
 	glfwTerminate(); // 不要忘记释放glfw资源
 	deleteSound(s1);
 	deleteSound(s2);
-	delete(camera);
+	delete (camera);
 	music.systemFree();
 	return 0;
 }
 // 回调函数
-void frame_size_callback(GLFWwindow* window, int width, int height)
+void frame_size_callback(GLFWwindow *window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
 	if (key == (GLFW_KEY_P) && action == GLFW_PRESS)
 	{
@@ -243,12 +248,12 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 		FMOD_Channel_Stop(s1->channel);
 	}
 }
-void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset)
 {
 	camera->scrollCallback(yoffset);
 }
 // 普通函数
-void press_close_window(GLFWwindow* window)
+void press_close_window(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, TRUE);
--- glLearn/res/shader/model.frag
+++ glLearn/res/shader/model.frag
@@ -1,8 +1,7 @@
 #version 400 core
 struct Material{
-    vec3 ambient;//材质的环境光反射率
-    vec3 diffuse;//材质的漫射光反射率
-    vec3 specular;//材质的镜面光反射率
+    sampler2D diffuseTexture;//材质的漫射光反射贴图
+    sampler2D specularTexture;//材质的镜面光反射贴图
     float shininess;//材质的反光度
 };
 struct Light{
@@ -14,24 +13,27 @@ struct Light{
 in vec2 texPos;
 in vec3 vertexPos;//未标准化(观察空间)
 in vec3 normal;//已标准化的法向量坐标(观察空间)
-uniform sampler2D ourTexture[6];//纹理
-uniform int texIndex;//纹理索引
 uniform Material material;//材质属性
+uniform sampler2D emission;//放射光材质
+uniform float yOffset;
+uniform float mixVector;
 uniform Light light;//光源属性
 out vec4 fragColor;
 void main()
 {
     //环境光
-    vec3 ambient=material.ambient*light.ambient;
+    vec3 ambient=light.ambient*texture(material.diffuseTexture,texPos).rgb;
     //漫射光
     vec3 VertexToLight=normalize(light.position-vertexPos);//顶点到光源矢量(注意不要先标准化,否则相减的矢量方向不正确)
     float diff=max(dot(normal,VertexToLight),0.0f);
-    vec3 diffuse=diff*material.diffuse*light.diffuse;
+    vec3 diffuse=diff*light.diffuse*texture(material.diffuseTexture,texPos).rgb;
     //镜面光
     vec3 reflection=reflect(-VertexToLight,normal);//因为入射向量和法向量均标准化，故无需标准化
     vec3 VertexToViewer=-normalize(vertexPos);
     float spec=pow(max(dot(reflection,VertexToViewer),0.0f),material.shininess);
-    vec3 specular=spec*material.specular*light.specular;
+    vec3 specular=spec*light.specular*texture(material.specularTexture,texPos).rgb;
+    //放射光
+    vec3 Emission=mixVector*texture(emission,texPos+vec2(0.0f,yOffset)).rgb;
     //输出颜色
-    fragColor=texture(ourTexture[texIndex],texPos)*vec4(ambient+diffuse+specular,1.0f);
+    fragColor=vec4(ambient+diffuse+specular+Emission,1.0f);
 }
