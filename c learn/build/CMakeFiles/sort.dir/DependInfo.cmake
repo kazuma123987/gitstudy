e506d40771e3f647441e462ca92697a2905ccc3a
kazuma123987 <142557221+kazuma123987@users.noreply.github.com>
2024-01-28 01:20:27 +0800

    [c learn]修复chess.exe不能运行的bug

--- c learn/build/CMakeFiles/sort.dir/DependInfo.cmake
+++ c learn/build/CMakeFiles/sort.dir/DependInfo.cmake
@@ -8,8 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/34181/Desktop/gitstudy/c learn/src/sort/main.c" "CMakeFiles/sort.dir/src/sort/main.c.obj" "gcc" "CMakeFiles/sort.dir/src/sort/main.c.obj.d"
   "C:/Users/34181/Desktop/gitstudy/c learn/src/sort/my_sort.c" "CMakeFiles/sort.dir/src/sort/my_sort.c.obj" "gcc" "CMakeFiles/sort.dir/src/sort/my_sort.c.obj.d"
-  "C:/Users/34181/Desktop/gitstudy/c learn/src/sort/main.cpp" "CMakeFiles/sort.dir/src/sort/main.cpp.obj" "gcc" "CMakeFiles/sort.dir/src/sort/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- c learn/build/CMakeFiles/sort.dir/cmake_clean.cmake
+++ c learn/build/CMakeFiles/sort.dir/cmake_clean.cmake
@@ -2,14 +2,14 @@ file(REMOVE_RECURSE
   "C:/Users/34181/Desktop/gitstudy/c learn/bin/sort.exe"
   "C:/Users/34181/Desktop/gitstudy/c learn/bin/sort.exe.manifest"
   "C:/Users/34181/Desktop/gitstudy/c learn/bin/sort.pdb"
-  "CMakeFiles/sort.dir/src/sort/main.cpp.obj"
-  "CMakeFiles/sort.dir/src/sort/main.cpp.obj.d"
+  "CMakeFiles/sort.dir/src/sort/main.c.obj"
+  "CMakeFiles/sort.dir/src/sort/main.c.obj.d"
   "CMakeFiles/sort.dir/src/sort/my_sort.c.obj"
   "CMakeFiles/sort.dir/src/sort/my_sort.c.obj.d"
   "libsort.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
+foreach(lang C)
   include(CMakeFiles/sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
